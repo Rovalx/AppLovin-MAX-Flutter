26bf57a1eda6835435c16eeaae41cd3dfbb53525
Dominik Majda <ddmajda@gmail.com>
2023-11-27 13:39:53 +0100

    Fix debug

--- applovin_max/ios/Classes/AppLovinMAX.m
+++ applovin_max/ios/Classes/AppLovinMAX.m
@@ -161,7 +161,7 @@ static FlutterMethodChannel *ALSharedChannel;
     settings.termsAndPrivacyPolicyFlowSettings.enabled = self.consentFlowEnabledToSet.boolValue;
     settings.termsAndPrivacyPolicyFlowSettings.privacyPolicyURL = self.privacyPolicyURLToSet;
     settings.termsAndPrivacyPolicyFlowSettings.termsOfServiceURL = self.termsOfServiceURLToSet;
-    settings.termsAndPrivacyPolicyFlowSettings.debugUserGeography = [self toAppLovinConsentFlowUserGeography: self.debugUserGeographyToSet];
+    AppLovinSdkSettings.termsAndPrivacyPolicyFlowSettings.debugUserGeography = [self toAppLovinConsentFlowUserGeography: self.debugUserGeographyToSet];
 
     self.consentFlowEnabledToSet = nil;
     self.privacyPolicyURLToSet = nil;
